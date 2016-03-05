d063b4c0105fd7dd48a3a1831e5f86da940c3f86
Isaac <isaac.durand@gmail.com>
2016-03-04 19:24:27 -0800

    Adding the first lesson

---
+++ lessons/git-on-your-computer.js
@@ -0,0 +1,27 @@
+// I'm importing React so I can use JSX.
+import React from 'react';
+
+// Export and array. Alternatively, we could use a linked list.
+
+export default [
+	{
+		lessonText: <h2>Hey howdy hey</h2>,
+		buttonText: 'button uno'
+	}, {
+		lessonText: 'i love that song!',
+		buttonText: 'button dos'
+	}
+];
+
+// ' Stop the madness
+
+// <h2>Welcome!</h2>
+// 			<p>If you're learning to code, chances are you've heard about something called Git. Git can be intimidating for beginners - but it doesn't have to be!</p>
+// 			<p>In this lesson, you'll...</p>
+// 			<ul>
+// 				<li>Set up Git</li>
+// 				<li>Set up a project</li>
+// 				<li>Learn some basic Git commands that you can use to track your new project</li>
+// 			</ul>
+// 			<p>Don't worry - we'll walk you through each step. Ready to get started?</p>
+// 			<button className='button-primary' id='lesson-button' onClick={function() {console.log('You clicked me!');}}>I don't do anything!</button>
