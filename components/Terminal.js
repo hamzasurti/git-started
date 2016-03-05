5a4dc5adfe28c8aff47b1e4aa89a2f67784032b2
hamzasurti <hamza.surti1@gmail.com>
2016-03-05 14:08:21 -0800

    terminal rendering in proper place. Still some issues with sendin and revieving data

--- components/Terminal.js
+++ components/Terminal.js
@@ -1,4 +1,7 @@
 import React, {Component} from 'react';
+const Term = require('term.js');
+
+
 
 // I can't get app.js to run here. In fact, the script tag doesn't object if I give it an invalid address like 'tomato'. We can add this script back after the form element if needed: <script src='./../app'></script>
 export default class Terminal extends Component {
@@ -10,7 +13,7 @@ export default class Terminal extends Component {
 				<form>
 					<input type="text" autoComplete="on" />
 				</form>
-				
+
       </div>
     )
   }
