77a0996b9cea8a190a8904ac4060cf60b6192570
hamzasurti <hamza.surti1@gmail.com>
2016-03-14 10:59:44 -0700

    working animation and almost functioning terminal

--- components/Terminal.js
+++ components/Terminal.js
@@ -8,12 +8,6 @@ export default class Terminal extends Component {
   render() {
     return (
       <div id='Terminal'>
-      	Terminal
-      	<div className="current"></div>
-				<form>
-					<input type="text" autoComplete="on" />
-				</form>
-
       </div>
     )
   }
--- main.js
+++ main.js
@@ -5,12 +5,10 @@ const app = electron.app;
 const ipcMain = require('electron').ipcMain;
 const	BrowserWindow = electron.BrowserWindow;
 const pty = require('pty.js');
+const net = require('net');
 
 var mainWindow = null;
 // What does it mean for a JS object to be garbage collected?
-
-// console.log('electron is', electron); // Electron seems to be defined correctly in both my terminal and the Electron console.
-// console.log('app is', app); // Weird - app is defined correctly in my terminal, but it's undefined in the Electron console.
 
 app.on('window-all-closed', function() {
 	if (process.platform !== 'darwin') {
@@ -23,30 +21,22 @@ app.on('ready', function() {
 	mainWindow.loadURL('file://' + __dirname + '/index.html');
 
 
-	var term = pty.spawn('bash', [], {
-	  name: 'xterm-color',
-	  cols: 80,
-	  rows: 50,
-	  cwd: process.env.HOME,
-	  env: process.env
-	});
-
-
-
-
-	var currentDirectory = ""
-console.log(process.env._);
-	ipcMain.on('command-message', function(event, arg) {
-
-		term.write(arg);
-		// term.resize(100, 40);
-		term.write(' && ls\r');
-		term.write('&& basename ' +  process.env._);
-		term.on('data', function(data) {
-			event.sender.send('terminal-reply', data);
-		});
-		// term.end()
-	});
+	// var ptyTerm = pty.fork('bash', [], {
+	//   name: 'xterm-color',
+	//   cols: 80,
+	//   rows: 50,
+	//   cwd: process.env.HOME,
+	//   env: process.env
+	// });
+	//
+	//
+	// ipcMain.on('command-message', function(event, arg) {
+	// 	ptyTerm.write(arg);
+	//
+	// 	ptyTerm.on('data', function(data) {
+	// 		event.sender.send('terminal-reply', data);
+	// 	});
+	// });
 
 	// For testing only
 	mainWindow.webContents.openDevTools();
