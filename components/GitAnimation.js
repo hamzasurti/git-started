0926fe84322c1317c0fb9bb14c64eae98cbd850e
Isaac <isaac.durand@gmail.com>
2016-03-22 17:08:23 -0700

    StructureAnimation is almost working correctly, but subsequent renders aren't calling updateTree

--- components/GitAnimation.js
+++ components/GitAnimation.js
@@ -4,7 +4,7 @@ export default class GitAnimation extends Component {
   render() {
     return (
       //JSX
-      <div>Git Animation</div>
+      <div>We will show a Git animation here</div>
     )
   }
 }
