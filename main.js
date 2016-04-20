e9927e07505a353dc867a50aab1a0af3a2e435a2
Isaac <isaac.durand@gmail.com>
2016-04-20 16:10:02 -0700

    Lint main.js

--- main.js
+++ main.js
@@ -6,7 +6,7 @@
 const electron = require('electron');
 const app = electron.app;
 const ipcMain = require('electron').ipcMain;
-const	BrowserWindow = electron.BrowserWindow;
+const BrowserWindow = electron.BrowserWindow;
 const animationDataSchema = require('./AnimationData/StructureSchema');
 const async = require('async');
 
@@ -15,6 +15,57 @@ const exec = require('child_process').exec;
 const fork = require('child_process').fork;
 
 let mainWindow = null;
+
+function ptyChildProcess() {
+  const ptyInternal = require.resolve('./ptyInternal');
+  const forkProcess = fork(ptyInternal);
+
+  // Prevent the app from showing dummy data on initial load.
+  ipcMain.on('ready-for-schema', (event, arg) => forkProcess.send({ message: arg }));
+
+  // Ensure that we have Git data to show the first time the users toggles to the Git view.
+  ipcMain.on('ready-for-git', (event, arg) => forkProcess.send({ message: arg }));
+
+  // For the lesson, ensure that we know the user's current directory before testing whether they
+  // created a 'new-project' directory.
+  ipcMain.on('ready-for-dir', (event, arg) => forkProcess.send({ message: arg }));
+
+  // When user inputs data in terminal, start fork and run pty inside.
+  // Each keystroke is an arg.
+  ipcMain.on('command-message', (event, arg) => {
+    forkProcess.send({ message: arg });
+    forkProcess.removeAllListeners('message');
+    forkProcess.on('message', (message) => {
+      // Send what is diplayed in terminal
+      if (message.data) event.sender.send('terminal-reply', message.data);
+      // Send animation schema
+      if (message.schema) event.sender.send('direc-schema', message.schema);
+      // Send Git data
+      if (message.gitGraph) event.sender.send('git-graph', message.gitGraph);
+      // Send current directory
+      if (message.currDir) event.sender.send('curr-dir', message.currDir);
+    });
+  });
+}
+
+// Run the appropriate test to see whether the user is ready to advance in the lesson.
+function slideTests() {
+  // Listen for commands from the lesson file.
+  ipcMain.on('command-to-run', (event, arg) => {
+    // Upon receiving a command, run it in the terminal.
+    exec(arg, (err, stdout) => {
+      // Send the terminal's response back to the lesson.
+      if (err) return event.sender.send('terminal-output', err.toString());
+      return event.sender.send('terminal-output', stdout);
+    });
+  });
+
+  // Listen for a Boolean from the lesson file.
+  ipcMain.on('test-result-1', (event, arg) => {
+    // Upon receiving it, send it to the Dashboard.
+    event.sender.send('test-result-2', arg);
+  });
+}
 
 
 app.on('window-all-closed', () => {
@@ -38,7 +89,7 @@ app.on('ready', () => {
 
   mainWindow.loadURL(`file://${__dirname}/index.html`);
 
-	// Initialize fork
+  // Initialize fork
   mainWindow.webContents.on('did-finish-load', () => {
     setTimeout(async.waterfall([
       async.apply(animationDataSchema.dataSchema, process.env.HOME),
@@ -47,98 +98,14 @@ app.on('ready', () => {
       },
     ]), 0);
 
-
     mainWindow.webContents.send('term-start-data', `${process.env.HOME} $ `);
 
     ptyChildProcess();
     slideTests();
   });
 
-	// For testing only, opens dev tools
-  // mainWindow.webContents.openDevTools();
-
-	// Set mainWindow back to null when the window is closed.
+  // Set mainWindow back to null when the window is closed.
   mainWindow.on('closed', () => {
     mainWindow = null;
   });
 });
-
-// This isn't running.
-// function initialLoadEvents(){
-// 	// when window finished loading, send current directory and animation structure
-// 	mainWindow.webContents.on('did-finish-load', () => {
-// 		async.waterfall([
-// 			async.apply(animationDataSchema.DataSchema, process.env.HOME),
-// 			(data) => { mainWindow.webContents.send('direc-schema', data);
-// 		}
-// 	]);
-// 	});
-// }
-
-
-function ptyChildProcess() {
-  const ptyInternal = require.resolve('./ptyInternal');
-  const forkProcess = fork(ptyInternal);
-
-	// Note from Isaac: I added this listener to prevent the app from loading our
-	//  dummy data on initial load.
-  ipcMain.on('ready-for-schema', (event, arg) => {
-			// console.log('Main.js received ready-for-schema');
-    forkProcess.send({ message: arg });
-			// Previously, we removed all listeners here. However, this prevented
-			//  main.js from sending a schema when the user toggles from the Git
-			//  animation to the structure animation.
-  });
-
-  // Note from Isaac: I added this listener to make we have Git data to show the first time the
-  // users toggles to the Git view.
-  ipcMain.on('ready-for-git', (event, arg) => forkProcess.send({message: arg}));
-
-	// Note from Isaac: I added this listener to ensure that the lesson knows the user's current
-  // directory before testing whether they created a 'new-project' directory.
-  ipcMain.on('ready-for-dir', (event, arg) => {
-    forkProcess.send({ message: arg });
-  });
-
-	// when user inputs data in terminal, start fork and run pty inside
-	// Each keystroke is an arg.
-  ipcMain.on('command-message', (event, arg) => {
-    forkProcess.send({ message: arg });
-    forkProcess.removeAllListeners('message');
-    forkProcess.on('message', (message) => {
-			// sends what is diplayed in terminal
-      if (message.data) event.sender.send('terminal-reply', message.data);
-			// sends animation schema
-      if (message.schema) event.sender.send('direc-schema', message.schema);
-
-      if (message.gitGraph) event.sender.send('git-graph', message.gitGraph);
-
-      if (message.currDir) event.sender.send('curr-dir', message.currDir);
-    });
-  });
-}
-
-// For running tests to see whether the user is ready to advance
-function slideTests() {
-	// Listen for commands from the lesson file.
-  ipcMain.on('command-to-run', (event, arg) => {
-		// Upon receiving a command, run it in the terminal.
-    exec(arg, (err, stdout) => {
-			// Send the terminal's response back to the lesson.
-      if (err) {
-			// This will give me the human-readable text description of the error from
-			//  the Error object.
-        console.log(err.toString());
-      } else {
-        console.log('terminal output:', stdout);
-      }
-      if (err) return event.sender.send('terminal-output', err.toString());
-      event.sender.send('terminal-output', stdout);
-    });
-  });
-	// Listen for a Boolean from the lesson file.
-  ipcMain.on('test-result-1', (event, arg) => {
-		// Upon receiving it, send it to the Dashboard.
-    event.sender.send('test-result-2', arg);
-  });
-}
