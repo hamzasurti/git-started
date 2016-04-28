843896120cc8d0d4f573aca52c377fcc89bf1404
Isaac <isaac.durand@gmail.com>
2016-04-27 19:53:52 -0700

    Remove extraneous comments

--- AnimationData/gitData.js
+++ AnimationData/gitData.js
@@ -4,8 +4,6 @@ const exec = require('child_process').exec;
 
 module.exports = {
   gitHistory(pwd) {
-    // const that = this;
-    // We can remove the max count below if desired: --max-count=10
     const commandForGitHistory = `cd ${pwd};git log --pretty="%h|%p|%s|%d"gi`;
     exec(commandForGitHistory, (err, stdout) => {
       if (err) {
@@ -13,13 +11,10 @@ module.exports = {
       } else {
         // splits stdout git log string by new line character
         const stdoutArr = stdout.split('\n');
-        // makes each string in stdoutArr a subarray slit at |
+        // makes each string in stdoutArr a subarray split at |
         const nestedCommitArr = stdoutArr.map((element) => element.split('|'));
-        // nestedCommitArr = nestedCommitArr.reverse();
-        // var gitGraph = that.createGraph(nestedCommitArr);
         const graphObj = { gitGraph: nestedCommitArr };
         process.send(graphObj);
-        // console.log(graphObj);
       }
     });
   },
--- tests/tests.js
+++ tests/tests.js
@@ -1,21 +1,20 @@
 /* eslint-disable max-len */
 /* eslint-disable no-unused-expressions */
-/* eslint-disable no-undef */
-// Mocha defines describe and it.
+/* eslint-env mocha */
 
 const expect = require('chai').expect;
-const mainFile = require('../AnimationData/StructureSchema');
+const StructureSchema = require('../AnimationData/StructureSchema');
 
-describe('inside StructureSchema', () => {
+describe('StructureSchema', () => {
   it('should have a dataSchema key', () => {
-    expect(mainFile.dataSchema).to.exist;
+    expect(StructureSchema.dataSchema).to.exist;
   });
   it('should return a schema that has a name property when schemaMaker is called', () => {
-    const schemaResult = mainFile.schemaMaker(['stdout', 'banana'], process.env.HOME)[0];
+    const schemaResult = StructureSchema.schemaMaker(['stdout', 'banana'], process.env.HOME)[0];
     expect(schemaResult).to.have.property('name', process.env.HOME);
   });
-  it('schemaMaker should return a schema that has a children property thats an array with a name stdout', () => {
-    const schemaResult = mainFile.schemaMaker(['stdout', 'banana'], process.env.HOME)[0];
+  it('schemaMaker should return a schema that has a children property that is an array with a name stdout', () => {
+    const schemaResult = StructureSchema.schemaMaker(['stdout', 'banana'], process.env.HOME)[0];
     expect(schemaResult).to.have.deep.property('children[0].name', 'stdout');
   });
 });
